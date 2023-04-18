5d736143f111b66b9a61d1d4dac775409642aea5
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-04-19 00:11:14 +0100

    refactor Order Tests

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -21,10 +21,18 @@ describe('Order Unit Test', () => {
   })
 
   it('Should calculate total', () => {
-    const item1 = new OrderItem("i1", "Item 1", 234, 4)
-    const item2 = new OrderItem("i2", "Item 2", 234, 4)
-    const order = new Order("123", "123", [item1, item2])
+    const item1 = new OrderItem("i1", "Item 1", 100, "p1", 2)
+    const item2 = new OrderItem("i2", "Item 2", 200, "p2", 3)
+    const order = new Order("o1", "c1", [item1, item2])
     const total = order.total()
-    expect(total).toBe(468)
+    expect(total).toBe(800)
+  })
+
+  it('Should throw error if item quantity is less than zero', () => {
+    expect(() => {
+      const item1 = new OrderItem("i1", "Item 1", 100, "p1", 0)
+      const order = new Order("o1", "c1", [item1])
+      const total = order.total()
+    }).toThrowError("Quantity must be greater than zero")
   })
 })
--- src/entity/order.ts
+++ src/entity/order.ts
@@ -24,10 +24,13 @@ export default class Order {
     if(this._items.length === 0) {
       throw new Error("Items are requrired")
     }
+    if (this._items.some(item => item.quantity <= 0)) {
+      throw new Error("Quantity must be greater than zero")
+    }
     return true
   }
 
   total():number {
-    return this._items.reduce((acc, item) => acc+item.price, 0)
+    return this._items.reduce((acc, item) => acc+item.total(), 0)
   }
 }
