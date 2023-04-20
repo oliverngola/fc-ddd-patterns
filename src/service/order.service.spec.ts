4596215f75e90876a77a91c5c58dcb0a6325aba6
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-04-20 23:00:24 +0100

    feat add OrderService Test

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,5 +1,14 @@
-describe('OrderService Unit Tests', () => {
-  test('', () => {
+import Order from "../entity/order"
+import OrderItem from "../entity/order_item"
+import OrderService from "./order.service"
 
+describe('OrderService Unit Tests', () => {
+  it('Should get total of all orders', () => {
+    const orderItem1 = new OrderItem("i1", "Item 1", 200, "p1", 1)
+    const orderItem2 = new OrderItem("i2", "Item 2", 50, "p2", 1)
+    const order1 =  new Order("o1", "c1", [orderItem1, orderItem2])
+    const order2 =  new Order("o1", "c2", [orderItem2])
+    const total = OrderService.total([order1, order2])
+    expect(total).toBe(300)
   })
 })
