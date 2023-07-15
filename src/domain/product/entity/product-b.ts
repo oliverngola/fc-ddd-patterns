1019170a495eab1ae7cd22ea2ae836e6693f219d
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-07-15 01:51:30 +0100

    add product factory helper method

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,6 +1,6 @@
 import type ProductInterface from './product.interface'
 
-export default class Product implements ProductInterface {
+export default class ProductB implements ProductInterface {
   private readonly _id: string
   private _name: string
   private _price: number
@@ -31,7 +31,7 @@ export default class Product implements ProductInterface {
   }
 
   get price (): number {
-    return this._price
+    return this._price * 2
   }
 
   validate (): boolean {
